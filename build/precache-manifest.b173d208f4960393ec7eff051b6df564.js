f4ad53d0721beb83ce115f22ca7317cb5cdc2c52
suicide11 <b117045@iiit-bh.ac.in>
2020-06-06 16:49:29 +0530

    chrome extension made

---
+++ build/precache-manifest.b173d208f4960393ec7eff051b6df564.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b575bb712471d945ea5f3b0de8041c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6faf269e331112579ba6",
+    "url": "/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "533588da447d02c0652b",
+    "url": "/static/css/main.e1d75783.chunk.css"
+  },
+  {
+    "revision": "6faf269e331112579ba6",
+    "url": "/static/js/2.8946897e.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.8946897e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533588da447d02c0652b",
+    "url": "/static/js/main.cb37a6a7.chunk.js"
+  },
+  {
+    "revision": "97b2976763cf47e27142",
+    "url": "/static/js/runtime-main.6486de84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
